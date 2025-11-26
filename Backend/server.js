70788617c20aada6611710d83e9c6bc52c196891
Shubham Zawar <shubhamzawar55@gmail.com>
2025-11-26 11:33:28 +0530

    editied server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
@@ -10,31 +11,43 @@ app.use(cors());
 
 app.listen(PORT,()=>{
   console.log(`server running on ${PORT}`);
+  connectDB();
 });
 
-
-app.post("/test",async (req,res)=>{
-  const options = {
-    method:"POST",
-    headers:{
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body:JSON.stringify({
-      model:"gpt-4o-mini",
-      messages:[{
-        role:"user",
-        content:req.body.message
-      }]
-    })
-  }
+const connectDB = async()=>{
   try
   {
-    const response = await fetch("https://api.openai.com/v1/chat/completions",options);
-    const data = await response.json();
-    // console.log(data);
-    res.send(data.choices[0].message.content);
-  }catch(err){
-    console.log(err);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with database");
+  }catch(err)
+  {
+    console.log("Failed to connect with DB",err);
   }
-})
+}
+
+
+// app.post("/test",async (req,res)=>{
+//   const options = {
+//     method:"POST",
+//     headers:{
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//     },
+//     body:JSON.stringify({
+//       model:"gpt-4o-mini",
+//       messages:[{
+//         role:"user",
+//         content:req.body.message
+//       }]
+//     })
+//   }
+//   try
+//   {
+//     const response = await fetch("https://api.openai.com/v1/chat/completions",options);
+//     const data = await response.json();
+//     // console.log(data);
+//     res.send(data.choices[0].message.content);
+//   }catch(err){
+//     console.log(err);
+//   }
+// })
