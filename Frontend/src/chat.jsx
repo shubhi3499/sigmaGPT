c22974fcb1a93fc89ad82a41572d96c8e268f488
Shubham Zawar <shubhamzawar55@gmail.com>
2025-12-08 10:43:16 +0530

    added a typing effect

--- Frontend/src/chat.jsx
+++ Frontend/src/chat.jsx
@@ -1,17 +1,38 @@
 import './chat.css';
-import { useContext } from 'react';
+import { useContext, useState,useEffect } from 'react';
 import { MyContext } from './MyContext';
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
 function chat(){
-    const {newChat,prevChats} = useContext(MyContext);
+    const {newChat,prevChats,reply} = useContext(MyContext);
+    const [latestReply,setLatestReply] = useState(null);
+
+    useEffect(()=>{
+        //latest reply seperate => typing effect 
+        if(!prevChats?.length)
+            return;
+
+        const content = reply.split(" ");
+
+        let idx = 0;
+        const interval = setInterval(()=>{
+            setLatestReply(content.slice(0,idx+1).join(" "));
+
+            idx++;
+            if(idx >= content.length)
+                clearInterval(interval);
+        },40);
+
+        return ()=> clearInterval(interval);
+
+    },[prevChats,reply])
     return(
         <>
             {newChat && <h1>Start a new Chat!</h1>}
             <div className="chats">
                 {
-                    prevChats?.map((chat,idx)=>
+                    prevChats?.slice(0,-1).map((chat,idx)=>
                         <div className={chat.role === "user"?"userDiv":"gptDiv"} key={idx}>
                             {
                                 chat.role === "user"?
@@ -21,6 +42,12 @@ function chat(){
                             }
                         </div>
                     )
+                }
+                {
+                    prevChats.length > 0 && latestReply != null &&
+                    <div className='gptDiv' key={"typing"}>
+                        <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown>
+                    </div>
                 }
                 
             </div>
